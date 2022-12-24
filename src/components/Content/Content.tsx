a06d5cd3e1d07c0dea2d4d7ae66c7507f01ba8d6
Ivan <vano5964307@gmail.com>
2022-12-24 18:41:09 +0400

    init: continue to create components and compilate pages

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,24 +1,39 @@
 import styles from "./content.module.css";
+import { avatar, banner } from "../../assets/assets";
+import { Button } from "../Button/Button";
+import { ProfileItem } from "../ProfileItem/ProfileItem";
+import { PostItem } from "../PostItem/PostItem";
 
 export const Content = () => {
   return (
     <div className={styles.content}>
-      <div>
-        Banner
-        <img />
+      <div className={styles.bannerWrap}>
+        <img className={styles.bannerWrapImg} src={banner} alt="banner" />
       </div>
-      <div>
-        Private data
-        <div>
-          <img src="#" alt="avatar" />
+      <div className={styles.privateData}>
+        <div className={styles.avatarWrap}>
+          <img className={styles.avatarWrapImg} src={avatar} alt="avatar" />
         </div>
-        <div>personal info</div>
+        <div className={styles.privateDataInfo}>
+          <div className={styles.privateDataInfoName}>John Dick</div>
+          <div className={styles.privateDataInfoContent}>
+            <ProfileItem name="Birth" value="June 21" />
+            <ProfileItem name="City" value="Polotsk" />
+            <ProfileItem name="Education" value='BSEU' />
+            <ProfileItem name='Website' value='js.com' />
+          </div>
+        </div>
       </div>
-      <div>
+      <div className={styles.myPosts}>
         My Posts
-        <div>new post</div>
-        <div>post list</div>
+        <div className={styles.myPostsNew}>
+          <input type="text"></input>
+          <Button onClick={() => {}} name='Send' />
+        </div>
+        <div className={styles.MyPostsList}>
+          <PostItem text="hi! I`m here for any entertainment" />
+        </div>
       </div>
     </div>
   );
-};
+}
